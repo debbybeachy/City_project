6d970edf9cc7c846640578633f9d8a2b87214a27
debbybeachy <letscaps07@gmail.com>
2014-08-18 18:53:15 -0500

    8/18/2014
    This is the second commit

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
- 
- 
- 
-});
+   
+   //Attach evet listener to input for keyup.
+   $( ".type" ).click(function() {
+      //alert( "Handler for .click() called." );
+     $(keydown)(function(event) {
+        if (event.which ==33) {
+           addItem();
+        };
+        
+     });
+   });
+   
+   
+}); //end of .ready event
