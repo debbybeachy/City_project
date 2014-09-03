e2edb293a7e2ad2a6c5ef28a1727aa73a6103350
debbybeachy <letscaps07@gmail.com>
2014-09-02 20:55:26 -0500

    9/2/2014
    Finished jQuery

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,15 +1,34 @@
-$(document).ready(function(){
-   
-   //Attach evet listener to input for keyup.
-   $( ".type" ).click(function() {
-      //alert( "Handler for .click() called." );
-     $(keydown)(function(event) {
-        if (event.which ==33) {
-           addItem();
-        };
-        
-     });
+$(document).ready(function() {
+
+   var attachRemove = function() {
+     $('.xBox').click(function(event) {
+      $(event.target).parent().remove();
+    });
+   }
+
+   attachRemove();
+
+   $('form').on('submit', function(event) {
+      event.preventDefault();
+      var newCity = $('.type').val()
+      
+      if (newCity == '') {
+      }
+
+      else {
+         $('.hardList').append('<li><button class="xBox">X</button>' + newCity + '</li>');
+         $('.type').val('');
+         attachRemove();
+      }
+   });
+
+   $('.hardList').click(function(event) {
+      $(event.target).toggleClass('strike');
    });
    
-   
-}); //end of .ready event
+   $('.clearList').click(function(event) {
+      event.preventDefault();
+      $('ul').empty();
+   });
+
+});
